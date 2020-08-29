f85cfa95c149194ab944e79c95c23cd472e73c15
Khalil Himura <khalil.himura@gmail.com>
2020-08-30 04:03:47 +0800

    Include *build* folder

---
+++ build/precache-manifest.f53f984497efed24649f63211c81d639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6601c181c9dd2adbfc58652250d6cff5",
+    "url": "/tania-react/index.html"
+  },
+  {
+    "revision": "6e5a7c0adb86a2c62440",
+    "url": "/tania-react/static/css/main.b672e0e3.chunk.css"
+  },
+  {
+    "revision": "30f6a656732f8aa00891",
+    "url": "/tania-react/static/js/2.33786497.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tania-react/static/js/2.33786497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5a7c0adb86a2c62440",
+    "url": "/tania-react/static/js/main.35ab8a7f.chunk.js"
+  },
+  {
+    "revision": "9ea7aa202d1f68fab325",
+    "url": "/tania-react/static/js/runtime-main.81d5dff8.js"
+  }
+]);
